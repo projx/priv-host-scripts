5d71c22fa212cf158caad8e4b571a5fe117c4b42
PRJX-UK <1296359+projx@users.noreply.github.com>
2023-07-03 16:22:36 +0100

    updated

--- zsh_install.sh
+++ zsh_install.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+echo "Installing ZSH and Oh My Zsh, setting as default shell"
+echo "ZSH Installation: Install zsh and font deb packages"
 apt install -y git-core curl fonts-powerline zsh
-
 chsh -s $(which zsh)
 
 ## Install Oh My Zsh and plugins
+echo "Install ZSH: Oh My Zsh and plugins"
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 ## Install Powerlevel10k
+echo "ZSH Installation: Installing Powerlevel10k"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-sed -i 's/ZSH_THEME="[^"]*/ZSH_THEME="powerlevel10k\/powerlevel10k/g' .zshrc
+# sed -i 's/ZSH_THEME="[^"]*/ZSH_THEME="powerlevel10k\/powerlevel10k/g' .zshrc
+
+echo "ZSH Installation: Grabbing zshrc and p10k.zsh configuration files"
 wget https://raw.githubusercontent.com/projx/priv-host-scripts/main/zsh/zshrc_config -O ~/.zshrc
 wget https://raw.githubusercontent.com/projx/priv-host-scripts/main/zsh/p10k.zsh -O ~/.p10k.zsh
 
