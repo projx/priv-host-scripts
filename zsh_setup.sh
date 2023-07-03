8e85b5ee70ff5c8f346a06494ebbcbbc3b346b3b
PRJX-UK <1296359+projx@users.noreply.github.com>
2023-07-03 02:01:10 +0100

    updated

--- zsh_setup.sh
+++ zsh_setup.sh
@@ -10,15 +10,6 @@ git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/cust
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 sed -i 's/ZSH_THEME="[^"]*/ZSH_THEME="powerlevel10k\/powerlevel10k/g' .zshrc
-wget https://raw.githubusercontent.com/projx/priv-host-scripts/main/zshrc_config -O ~/.zshrc
+wget https://raw.githubusercontent.com/projx/priv-host-scripts/main/zsh/zshrc_config -O ~/.zshrc
+wget https://raw.githubusercontent.com/projx/priv-host-scripts/main/zsh/p10k.zsh -O ~/.p10k.zsh
 
-#Powerlevel10k configuration wizard has been aborted. It will run again next time unless
-#you define at least one Powerlevel10k configuration option. To define an option that
-#does nothing except for disabling Powerlevel10k configuration wizard, type the following
-#command:
-#
-#  echo 'POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true' >>! ~/.zshrc
-#
-#To run Powerlevel10k configuration wizard right now, type:
-#
-#  p10k configure
