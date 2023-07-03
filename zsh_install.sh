83f7e11d2f8045f0ef61d7eb2deef62bedfe6371
PRJX-UK <1296359+projx@users.noreply.github.com>
2023-07-03 18:42:22 +0100

    updated

--- zsh_install.sh
+++ zsh_install.sh
@@ -8,7 +8,7 @@ chsh -s $(which zsh)
 ## Install Oh My Zsh and plugins
 echo "Install ZSH: Oh My Zsh and plugins"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
 ## Install Powerlevel10k
 echo "ZSH Installation: Installing Powerlevel10k"
@@ -17,11 +17,8 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 # sed -i 's/ZSH_THEME="[^"]*/ZSH_THEME="powerlevel10k\/powerlevel10k/g' .zshrc
 
 echo "ZSH Installation: Grabbing zshrc and p10k.zsh configuration files"
-wget https://raw.githubusercontent.com/projx/priv-host-scripts/main/zsh/zshrc_config -O /root/.zshrc
-wget https://raw.githubusercontent.com/projx/priv-host-scripts/main/zsh/p10k.zsh -O /root/.p10k.zsh
-
-#wget https://raw.githubusercontent.com/projx/priv-host-scripts/main/zsh/zshrc_config -O /home/system/.zshrc
-#wget https://raw.githubusercontent.com/projx/priv-host-scripts/main/zsh/p10k.zsh -O /home/system/.p10k.zsh
+wget https://raw.githubusercontent.com/projx/priv-host-scripts/main/zsh/zshrc_config -O $HOME/.zshrc
+wget https://raw.githubusercontent.com/projx/priv-host-scripts/main/zsh/p10k.zsh -O $HOME/.p10k.zsh
 
 #wget https://raw.githubusercontent.com/projx/priv-host-scripts/main/zsh/zshrc_config -O $HOME/zshrc123
 #wget https://raw.githubusercontent.com/projx/priv-host-scripts/main/zsh/p10k.zsh -O ~/.p10k.zsh
