15f5e9860745587bd75c0dd1989952d93dbf7034
PRJX-UK <1296359+projx@users.noreply.github.com>
2023-08-28 01:41:31 +0100

    updated

--- kube_dns_update.py
+++ kube_dns_update.py
@@ -1,58 +1,106 @@
-from pathlib import Path
-import platform
-from pprint import pprint
+
+import socket
+import fcntl
+import struct
+import subprocess
 
 
-class HostBase():
-    def host_addresses(self):
-        return [
-                "127.0.0.1 localhost",
-                "10.10.110.22 nas-01",
-                "10.10.110.32 nas-02"
-        ]
+hosts = {
+    ## Base hosts
+    "127.0.0.1" : "localhost",
+    "10.10.110.22" : "nas-01",
+    "10.10.110.32" : "nas-02",
 
-class KubeHosts(HostBase):
+    ## GFS hosts
+    "10.10.110.71": "kube-gfs-01",
+    "10.10.110.72": "kube-gfs-02",
+    "10.10.110.73": "kube-gfs-03",
 
-    def host_addresses(self, host):
-        host_list = super().host_addresses()
+    ## Kube svr hosts
+    "10.10.40.51" : "ks-01",
+    "10.10.40.52" : "ks-02",
+    "10.10.40.53" : "ks-03",
+    "10.10.40.54" : "ks-05",
+    "10.10.40.55" : "ks-05",
+    "10.10.40.56" : "ks-06",
 
-        hosts = [
-                 "10.10.110.71 kube-gfs-01",
-                 "10.10.110.72 kube-gfs-02",
-                 "10.10.110.73 kube-gfs-03",
-
-                 "10.10.40.81 ks-01",
-                 "10.10.40.82 ks-02",
-                 "10.10.40.83 ks-03",
-                 "10.10.40.84 ks-05",
-                 "10.10.40.85 ks-05",
-                 "10.10.40.86 ks-06",
-
-                 "10.10.40.91 km-01"
-        ]
-
-        for v in hosts:
-            if host not in v:
-                print("Adding: {}".format(v))
-                host_list.append(v)
-            else:
-                entry = "127.0.1.1 " + host
-                print("Adding local: {}".format(entry))
-                host_list.append(entry)
-
-        return host_list
+    ## Kube mgr hosts
+    "10.10.40.61" : "km-01",
+    "10.10.40.62" : "km-02",
+    "10.10.40.63" : "km-03",
+    "10.10.40.64" : "km-04",
+    "10.10.40.65" : "km-05"
+}
 
 
-hostname = platform.node()
-host_file = "/etc/hosts"
+def get_primary_ip():
+    try:
+        # Get the list of network interfaces
+        interfaces = socket.if_nameindex()
 
-print("Current hostname is: " + hostname)
+        for _, interface_name in interfaces:
+            # Get the IP address of the interface
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+                ip_address = socket.inet_ntoa(fcntl.ioctl(
+                    s.fileno(),
+                    0x8915,  # SIOCGIFADDR
+                    struct.pack('256s', interface_name.encode('utf-8')[:15])
+                )[20:24])
 
-addr_list = KubeHosts().host_addresses(hostname)
-addr_str = "\n".join(addr_list)
-print("Dumping to file: \n" + host_file)
+            # Check if the IP address is not localhost (127.0.0.1)
+            if ip_address != '127.0.0.1':
+                return ip_address
 
-fh = open(host_file, 'a')
-fh.write("\n\n"+addr_str)
-fh.close()
-print("\n\n"+addr_str)
+        return None  # No non-localhost NIC found
+    except Exception as e:
+        print("An error occurred:", e)
+        return None
+
+
+def set_hostname(new_hostname):
+    try:
+        # Run the hostnamectl command to set the new hostname
+        subprocess.run(['sudo', 'hostnamectl', 'set-hostname', new_hostname], check=True)
+        print(f"Hostname set to '{new_hostname}' successfully.")
+    except subprocess.CalledProcessError as e:
+        print("Error:", e)
+        print("Failed to set the hostname.")
+    except Exception as e:
+        print("An unexpected error occurred:", e)
+
+def generate_host_list(dns_list, host_ip):
+    output = []
+    for ip, host in dns_list.items():
+        if ip != host_ip:
+            output.append("{} {}".format(ip, host))
+        else:
+            output.append("{} {}".format("127.0.0.1", host))
+        
+    return output
+
+def main():
+    host_file = "./test_hosts"
+
+    print("Looking up host primary IP address...")
+    ip = get_primary_ip()
+    if ip in hosts:
+        print("Found hostname: {} matches {}".format(ip, hosts[ip]))
+        set_hostname(hosts[ip])
+        hl = generate_host_list(hosts, ip)
+    
+        addr_str = "\n".join(hl)
+        print("Dumping to file: \n" + host_file)
+        
+        fh = open(host_file, 'a')
+        fh.write("\n\n"+addr_str)
+        fh.close()
+    else:
+        print("** ABORTING **\n Failed to get the IP address of the first network interface.")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+
+
--- zsh_install.sh
+++ zsh_install.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
-echo $1
-echo $2
-echo $3
 
-#echo "Installing ZSH and Oh My Zsh, setting as default shell"
-#echo "ZSH Installation: Install zsh and font deb packages"
-#apt install -y git-core curl fonts-powerline zsh
-#
-#
-### Install Oh My Zsh and plugins
-#echo "Install ZSH: Oh My Zsh and plugins"
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-#git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-#
-### Install Powerlevel10k
-#echo "ZSH Installation: Installing Powerlevel10k"
-#git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-#
-### Set the 10k theme...
-## sed -i 's/ZSH_THEME="[^"]*/ZSH_THEME="powerlevel10k\/powerlevel10k/g' .zshrc
-#
-#echo "ZSH Installation: Grabbing zshrc and p10k.zsh configuration files"
-#wget https://raw.githubusercontent.com/projx/priv-host-scripts/main/zsh/zshrc_config -O $HOME/.zshrc
-#wget https://raw.githubusercontent.com/projx/priv-host-scripts/main/zsh/p10k.zsh -O $HOME/.p10k.zsh
-#
-### Set the default shell
-##chsh -s $(which zsh)
-#echo "exec " $(which zsh) >> $HOME/.bashrc
-#
+echo "Installing ZSH and Oh My Zsh, setting as default shell"
+echo "ZSH Installation: Install zsh and font deb packages"
+apt install -y git-core curl fonts-powerline zsh
+
+
+## Install Oh My Zsh and plugins
+echo "Install ZSH: Oh My Zsh and plugins"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+## Install Powerlevel10k
+echo "ZSH Installation: Installing Powerlevel10k"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+## Set the 10k theme...
+# sed -i 's/ZSH_THEME="[^"]*/ZSH_THEME="powerlevel10k\/powerlevel10k/g' .zshrc
+
+echo "ZSH Installation: Grabbing zshrc and p10k.zsh configuration files"
+wget https://raw.githubusercontent.com/projx/priv-host-scripts/main/zsh/zshrc_config -O $HOME/.zshrc
+wget https://raw.githubusercontent.com/projx/priv-host-scripts/main/zsh/p10k.zsh -O $HOME/.p10k.zsh
+
+## Set the default shell
+#chsh -s $(which zsh)
+echo "exec " $(which zsh) >> $HOME/.bashrc
+
